206fc62542b4a7ee5e20d119b51aea9be9b80175
mitchgrout <mitchgrout@users.noreply.github.com>
2019-08-19 12:55:21 +1200

    Create infrastructure (#2)
    * Add terraform+ansible scripts for managing instances
    
    * Add scripts for making deployment simpler
    
    * Add placeholder content
    
    * Add basic instructions for usage
    
    * Missing ignore

---
+++ assignment1/deploy/vars.tf
@@ -0,0 +1,26 @@
+### NOTE: Nothing *sensitive* should be present here
+###       If required, move to auth.tf
+
+variable "whitelisted_ip_blocks" {
+    description = "List of known-good IPs to allow connections from"
+    type        = list(string)
+    default     = ["130.217.0.0/16", "118.92.51.119/32"]
+}
+
+variable "public_key_path" {
+    description = "Path to OpenSSH (RSA) public key"
+    type        = string
+    default     = "./deployer.pub"
+}
+
+variable "server_port" {
+    description = "Port used by our server"
+    type        = number 
+    default     = 80
+}
+
+variable "instance_count" {
+    description = "Number of EC2 instances to run"
+    type        = number
+    default     = 4
+}
