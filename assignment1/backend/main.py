mitchgrout/COMPX527

commit 0619557fb9bf0e66f0bf9c56f47715b8845ec87a

Backend security (#13)
* Return error with code 200 also

* Add rudimentary ratelimiting + means for testing to avoid being ratelimited

* Add missing dependency for ratelimiter

* Rebase against master (Add stickiness policy)

assignment1/backend/main.py
@@ -18,12 +18,25 @@ with open('image_info_test2017.json', 'r') as fd:
         info.append({name:elem[name] for name in ['file_name', 'coco_url', 'width', 'height']})
 
 # Set up Flask
-from flask import Flask, request, jsonify
+from flask              import Flask, request, jsonify
+from flask_limiter      import Limiter
+from flask_limiter.util import get_remote_address
 app = Flask(__name__)
+lim = Limiter(app, key_func=get_remote_address)
 j   = jsonify
+
+# Custom ratelimiter
+@app.errorhandler(429)
+def ratelimiter(e):
+    return j({ 'error': 'rate limited' }), 200
+
+# Custom exemption for testing
+def exemption():
+    return "no_limiter" in request.values
 
 # Hook up a route for POST /detect
 @app.route('/detect', methods=['POST'])
+@lim.limit("1/second;5/minute;25/hour", exempt_when=exemption)
 def detect_image():
     # Fetch and validate the thresh value
     thresh = 0.5
@@ -31,18 +44,18 @@ def detect_image():
         try:
             thresh = float(request.values['thresh'])
         except (ValueError, TypeError):
-            return j({ 'error': 'thresh was not a float' }), 400
+            return j({ 'error': 'thresh was not a float' })
     if thresh < 0.0:
-        return j({ 'error': '<0 threshold supplied' }), 400
+        return j({ 'error': '<0 threshold supplied' })
     elif thresh > 1.0:
-        return j({ 'error': '>1 threshhold supplied' }), 400
+        return j({ 'error': '>1 threshhold supplied' })
         
     # Fetch the image; this can be passed either as an uploaded image OR URL
     # NOTE: For sanity we only accept <2MB images
     if 'image' in request.files:
         stream = request.files['image'].stream.read(2 * 1024 * 1024)
         if len(request.files['image'].stream.read(1)):
-            return j({ 'error': 'input images are restricted to 2MB' }), 400
+            return j({ 'error': 'input images are restricted to 2MB' })
     elif 'url' in request.values:
         from requests import get
         resp = get(request.values['url'], stream=True)
@@ -50,12 +63,12 @@ def detect_image():
         read_chunk = False
         for chunk in resp.iter_content(2 * 1024 * 1024):
             if read_chunk:
-                return j({ 'error': 'input images are restricted to 2MB' }), 400
+                return j({ 'error': 'input images are restricted to 2MB' })
             by += chunk
             read_chunk = True
         stream = bytes(by)
     else:
-        return j({ 'error': 'no image supplied' }), 400
+        return j({ 'error': 'no image supplied' })
 
    # Try to parse the image
     image = None
@@ -65,7 +78,7 @@ def detect_image():
         from PIL import Image
         image = numpy.array(Image.open(BytesIO(stream)).convert('RGB'))
     except OSError as e:
-        return j({ 'error': 'could not parse image' }), 400
+        return j({ 'error': 'could not parse image' })
         
     # Everything from here *should be* safe, but we'll wrap it just in case
     try:
@@ -88,13 +101,14 @@ def detect_image():
             results.append({ 'score': float(score)
                            , 'label': label_map[label]
                            , 'box'  : [float(p) for p in box] })
-        return j({ 'success': results }), 200
+        return j({ 'success': results })
 
     except Exception as e:
-        return j({ 'error': 'fatal error: {} {}'.format(type(e), e) }), 500
+        return j({ 'error': 'fatal error: {} {}'.format(type(e), e) })
         # TODO: Try to reduce this?
 
 @app.route('/info', methods=['GET'])
+@lim.limit("10/second", exempt_when=exemption)
 def get_coco_info():
     # Parse the from/to params
     fr = None
@@ -104,16 +118,16 @@ def get_coco_info():
             fr = int(request.args['from'])
             fr = max(fr, 0)
         except (ValueError, TypeError):
-            return j({ 'error': 'from must be an integer' }), 400
+            return j({ 'error': 'from must be an integer' })
     if 'to' in request.args:
         try:
             to = int(request.args['to'])
             to = max(to, 0)
         except (ValueError, TypeError):
-            return j({ 'error': 'to must be an integer' }), 400
+            return j({ 'error': 'to must be an integer' })
 
     # Slice the pre-loaded info 
-    return j({ 'success': info[fr:to] }), 200
+    return j({ 'success': info[fr:to] })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

assignment1/backend/test_main.py
@@ -6,11 +6,11 @@ import json
 def detect(img=None, thresh=None):
     data  = {} if thresh is None else {'thresh':thresh}
     files = {} if img    is None else {'image': img}
-    return requests.post('http://127.0.0.1:8080/detect', files=files, data=data)
+    return requests.post('http://127.0.0.1:8080/detect?no_limiter', files=files, data=data)
 
 # Wrapper for GET /info
 def info():
-    return requests.get('http://127.0.0.1:8080/info')
+    return requests.get('http://127.0.0.1:8080/info?no_limiter')
 
 # Loads the file for the given input tag
 def test_file(tag):
@@ -20,13 +20,12 @@ def test_file(tag):
 # Determine if an HTTP response is an error
 def is_error(response):
     obj = json.loads(response.text)
-    return response.status_code != 200 and 'error' in obj
+    return response.status_code == 200 and 'error' in obj
 
 # Determine if an HTTP response is successful
 def is_success(response):
     obj = json.loads(response.text)
     return response.status_code == 200 and 'success' in obj
-
 
 class WebTest(unittest.TestCase):
     # Thresh should be in float [0,1]

assignment1/deploy/bootstrap_ec2.yml
@@ -46,7 +46,7 @@
 
     - name: Fetch Pip dependencies
       pip:
-        name: [flask, numpy, keras, tensorflow] 
+        name: [flask, flask_limiter, numpy, keras, tensorflow] 
 
     - name: Install keras_retinanet
       pip:

assignment1/deploy/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ec2_instances" {
     # ubuntu-xenial-16.04-amd64-server-20190605
     # TODO: Use custom AMI?
     ami           = "ami-01d9d5f6cecc31f85"
-    instance_type = "t2.2xlarge"
+    instance_type = "t2.medium"
 
     # Hook up SSH key and configure ports
     key_name        = aws_key_pair.deployer.key_name 
@@ -55,6 +55,14 @@ resource "aws_elb" "load_balancer"{
         interval            = 5
         target              = "TCP:${var.server_port}"
     }
+}
+
+# Attach a stickiness policy to help reduce abuse
+resource "aws_lb_cookie_stickiness_policy" "load_balancer_sticky" {
+    name                     = "lb-sticky-policy" 
+    load_balancer            = "${aws_elb.load_balancer.id}"
+    lb_port                  = 80
+    cookie_expiration_period = 300
 }
 
 # Attach our instances

assignment1/deploy/vars.tf
@@ -22,5 +22,5 @@ variable "server_port" {
 variable "instance_count" {
     description = "Number of EC2 instances to run"
     type        = number
-    default     = 2
+    default     = 3
 }

assignment1/html/detect.php
@@ -2,7 +2,7 @@
 
 # Init a curl connection to the original server
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://'.$_SERVER['SERVER_ADDR'].':8080/detect');
+curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:8080/detect');
 curl_setopt($ch, CURLOPT_POST, 1);
 $post = array();
 

assignment1/html/info.php
@@ -1,5 +1,5 @@
 <?php
-$url     = 'http://'.$_SERVER['SERVER_ADDR'].':8080/info?';
+$url = 'http://127.0.0.1:8080/info?';
 if (isset($_GET['from'])) $url = $url."from=".$_GET['from'].'&';
 if (isset($_GET['to']))   $url = $url."to="  .$_GET['to']  .'&';
 $options = array(

commit 68fe4ee1995a45ed03437187b206929e85e0743e

Fix typo introduced in pull #13

assignment1/backend/main.py
@@ -1,4 +1,4 @@
-in t#!/usr/bin/env python3
+#!/usr/bin/env python3
 
 # Set up our global model
 import tensorflow